e6aadcbe138c0cd81ebcbbd2b2400461a9211a71
BharatNaik24 <159719274+BharatNaik24@users.noreply.github.com>
2024-10-07 22:01:21 +0530

    Update ProductInfo.jsx

--- src/pages/productInfo/ProductInfo.jsx
+++ src/pages/productInfo/ProductInfo.jsx
@@ -14,7 +14,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { doc, getDoc } from "firebase/firestore";
 import { toast } from "react-toastify";
 import { addToCart } from "../../redux/cartSlice";
-import { fireDB } from "../../firebase/firebaseConfig";
+import { fireDB } from "../../firebase/FirebaseConfig";
 
 function ProductInfo() {
   const context = useContext(myContext);
